4aa1d4159b88774023d146c3a8be515cef9f7a92
Andrew Eroh <andrew.eroh@gmail.com>
2022-04-04 20:19:37 -0400

    First commit for other example files

---
+++ Welcome201.py
@@ -0,0 +1,4 @@
+# April 04, 2022
+# This program prints hello Python
+
+print("Hello Python world!")
---
+++ Welcome202.py
@@ -0,0 +1,8 @@
+# April 04, 2022
+# This program prints hello Python using a variable
+
+message = "Hello Python World!"
+print(message)
+
+message = "Hello Python Crash Course world!"
+print(message)
